523c7be4a22f645e994b7f3700406f233f546dc1
jmbohan <jmbohan@purdue.edu>
2021-07-24 00:35:25 -0400

    Hello world is a tired motherfucker

---
+++ GreatFun.java
@@ -0,0 +1,10 @@
+// his is another simple Java program.
+
+public class GreatFun
+{
+     public static void main(String[] args)
+     {
+          System.out.print("Programming is ");
+          System.out.println("great fun!");
+     }
+}
